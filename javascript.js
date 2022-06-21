4dc88a08744e6bfd8ec0db4f4f6c8439c99d210a
sarthak501 <103514010+sarthak501@users.noreply.github.com>
2022-06-21 16:04:53 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function find() {
+var k=document.getElementById("x").value;
+
+window.open("https://en.softonic.com/s/"+k,"_self");
+}
+/*nction AvoidSpace(event) {
+    var k = event ? event.which : window.event.keyCode;
+    if (k == 32) return false;
+}*/
